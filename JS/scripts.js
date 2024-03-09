587c5189f63d48e2d0d1276cd0d2fe859a7b3f0f
eli-souto-r <eli.souto.r@gmail.com>
2024-03-09 12:16:38 +0100

    home y sobre mi listo

--- JS/scripts.js
+++ JS/scripts.js
@@ -49,3 +49,22 @@ lupa.addEventListener("click", function() {
         
     }
 });
+
+/*flecha ariba */
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollBtn").style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    var scrollToTopBtn = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollToTopBtn > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, scrollToTopBtn - scrollToTopBtn / 8);
+    }
+}
